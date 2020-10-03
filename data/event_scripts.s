538295c6c5b080e36f38f1368b77f689774010a8
Kubes' Content <kubescontent@gmail.com>
2020-10-03 01:59:00 -0400

    Added Route1 mockup.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1062,3 +1062,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/text/birch_speech.inc"
 
 	.include "data/maps/HomeTown/scripts.inc"
+
+	.include "data/maps/Route1/scripts.inc"
